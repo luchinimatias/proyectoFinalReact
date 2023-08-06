7b1e34a1d449422236fe65df75e3e0b14ac1185d
luchinimatias <luchinimatiasfibercorp@gmail.com>
2023-08-06 16:30:24 -0300

    NadBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
-
+import Nav from "../src/components/nav/Nav";
 function App() {
-  
   return (
-    <div></div>
-  )
+    <div className="container">
+      <Nav
+        imageLogo="https://i.postimg.cc/prY6SW5x/isologotipo-imprimir-opcion-1.png"
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
